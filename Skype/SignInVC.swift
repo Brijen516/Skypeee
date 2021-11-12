7da6824b9a987d412acd7e42918d60ab14071086
ammenedeal <88452126+ammenedeal@users.noreply.github.com>
2021-11-12 12:27:05 +0530

    10

---
+++ Skype/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  SignInVC.swift
+//  Skype
+//
+//  Created by ABHI on 12/11/21.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var nameTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onClickSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onClickCreate(_ sender: Any) {
+    }
+}
